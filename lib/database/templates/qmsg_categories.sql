23d09a38c14c7bcad3be023bcd95bb88e4951614
webnull <webnull.www@gmail.com>
2013-08-31 00:38:17 +0200

    Fixed quickMessage database templates

--- lib/database/templates/qmsg_categories.sql
+++ lib/database/templates/qmsg_categories.sql
@@ -5,6 +5,8 @@ CREATE TABLE `{$db_prefix}qmsg_categories` (
   `title` varchar(128) NOT NULL,
   `description` varchar(256) NOT NULL,
   `category_name` varchar(16) CHARACTER SET latin1 NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `author_id` int(6) NOT NULL,
   PRIMARY KEY (`category_id`),
   UNIQUE KEY `category_name` (`category_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
--- lib/database/templates/sqlite3/qmsg_categories.sql
+++ lib/database/templates/sqlite3/qmsg_categories.sql
@@ -4,7 +4,9 @@ CREATE TABLE "{$db_prefix}qmsg_categories" (
   "category_id" INTEGER PRIMARY KEY AUTOINCREMENT,
   "title" TEXT NOT NULL,
   "description" TEXT NOT NULL,
-  "category_name" TEXT NOT NULL
+  "category_name" TEXT NOT NULL,
+  "created" datetime default current_timestamp,
+  "author_id" INTEGER NOT NULL
 );
 
 CREATE INDEX "{$db_prefix}qmsg_categories_category_name" ON "{$db_prefix}qmsg_categories" ("category_name");
