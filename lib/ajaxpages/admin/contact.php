df1f27875cd92efb4e661490131dc37b62e52f4b
webnull <webnull.www@gmail.com>
2013-09-13 20:38:37 +0200

    Added frontpage Contact API (issue #242)

--- lib/ajaxpages/admin/contact.php
+++ lib/ajaxpages/admin/contact.php
@@ -35,26 +35,28 @@ $panthera -> template -> push ('selected_language', $language);
 if ($_GET['action'] == 'save')
 {
     if (isset($_POST['all_langs']))
-        $panthera -> config -> setKey('contact_generic', True, 'bool');
+        $panthera -> config -> setKey('contact.generic', True, 'bool');
     else {
-        $panthera -> config -> setKey('contact_generic', False, 'bool');
+        $panthera -> config -> setKey('contact.generic', False, 'bool');
         
         $_GET['language'] = $language = $_POST['save_as_language'];  
     }
 }
 // if using one contact page for all languages
-if ($panthera->config->getKey('contact_generic', False, 'bool'))
+if ($panthera->config->getKey('contact.generic', False, 'bool'))
 {
-    $fieldName = 'contact_all';
+    $fieldName = 'contact.lang.all';
     $panthera -> template -> push ('oneContactPage', True);
 } else {
-    $fieldName = 'contact_' .$language;
+    $fieldName = 'contact.lang.' .$language;
 }
 
 
-$contactDefaults = array('text' => '<p>Paste contact data here</p>', 
-                             'map' => '{"bounds":{"Z":{"b":50.52538601346569,"d":50.78657485494268},"fa":{"b":17.58736379609377,"d":18.27400930390627}},"zoom":10,"center":{"jb":50.65616198748283,"kb":17.93068655000002}}', 
-                             'mail' => 'example@example.com');
+$contactDefaults = array(
+    'text' => '<p>Paste contact data here</p>', 
+    'map' => '{"bounds":{"Z":{"b":50.52538601346569,"d":50.78657485494268},"fa":{"b":17.58736379609377,"d":18.27400930390627}},"zoom":10,"center":{"jb":50.65616198748283,"kb":17.93068655000002}}', 
+    'mail' => 'example@example.com'
+);
                              
 $contactData = $panthera -> config -> getKey($fieldName, $contactDefaults, 'array');
 
@@ -76,21 +78,22 @@ if ($_GET['action'] == 'save')
     if (!defined('CONTACT_SKIP_MAP'))
     {
         // set map location data
-        if(isJson($mdata))
-            $contactData['map'] = $mdata;
-        else {
+        if(!isJson($mdata))
+        {
             ajax_exit(array('status' => 'failed', 'error ' => localize('Invalid map location data', 'contactpage')));
         }
+        
+        $contactData['map'] = $mdata;
     }
 
     if (strlen($email) > 0)
     {
-        if (filter_var($email, FILTER_VALIDATE_EMAIL))
-            $contactData['mail'] = $email;
-        else {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+        {
             ajax_exit(array('status' => 'failed', 'error ' => localize('Invalid e-mail adress', 'contactpage')));
-            pa_exit();
         }
+        
+        $contactData['mail'] = $email;
     }
     
     // save data right back to database
@@ -107,10 +110,23 @@ $html = htmlspecialchars($html, ENT_QUOTES);
 
 if (!defined('CONTACT_SKIP_MAP'))
 {
-    $map_data = json_decode(stripslashes($contactData['map']));
-    $template -> push ('map_zoom', $map_data->zoom);
-    $template -> push ('map_x', $map_data->center->jb);
-    $template -> push ('map_y', $map_data->center->kb);
+    $map_data = json_decode(stripslashes($contactData['map']), true);
+    $x = $map_data['center']['pb'];
+    $y = $map_data['center']['qb'];
+    $zoom = $map_data['zoom'];
+    
+    if (!$x)
+        $x = 0;
+        
+    if (!$y)
+        $y = 0;
+        
+    if (!$zoom)
+        $zoom = 1;
+        
+    $template -> push ('map_zoom', $zoom);
+    $template -> push ('map_x', $x);
+    $template -> push ('map_y', $y);
 } else
     $template -> push ('skip_map', True);
 
---
+++ lib/pages/contact.php
@@ -0,0 +1,40 @@
+<?php
+/**
+  * Custom pages example action handler
+  *
+  * @package Panthera\core\pages
+  * @author Damian Kęska
+  * @license GNU Affero General Public License 3, see license.txt
+  */
+
+// just in case
+$panthera -> importModule('contact');
+$contact = new contactFrontpage;
+
+// see documentation or module source code to get list of all variables that can be modified
+$contact -> topicTemplate = "[".$_SERVER['HTTP_HOST']."] {\$p_contactTopic}";
+$contact -> mailBody = localize('From', 'contactpage').": {\$p_contactName} < {\$p_contactMail} >\n\n".localize('Content', 'contactpage').":\n{\$p_contactContent}";
+$contact -> fields['p_contactJabber']['enabled'] = True;
+
+// test array with example input
+/*
+$data = array(
+    'p_contactMail' => 'my_address@mydomain.org',
+    'p_contactContent' => 'This is a test, this should be sent to my inbox, modify me to test form validation',
+    'p_contactName' => 'Jan Kowalski',
+    'p_contactTopic' => 'This is a topic',
+    'p_contactJabber' => 'jabber@example.org'
+);*/
+
+if (isset($_GET['send']))
+{
+    $result = $contact->handleData($_POST); // any array can be used to test the form
+    
+    if (is_array($result))
+    {
+        $panthera -> template -> display('p_contactErrorMsg', $result['error']);
+    }
+}
+
+$panthera -> template -> display('contact.tpl');
+pa_exit();
