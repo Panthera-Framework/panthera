9e9c10862729b019d84230f51724c9098f683364
Mateusz Warzyñski <lxnmen@gmail.com>
2013-10-17 01:39:37 +0200

    Change map default localization

--- lib/ajaxpages/admin/contact.php
+++ lib/ajaxpages/admin/contact.php
@@ -54,7 +54,7 @@ if ($panthera->config->getKey('contact.generic', False, 'bool'))
 
 $contactDefaults = array(
     'text' => '<p>Paste contact data here</p>', 
-    'map' => '{"bounds":{"Z":{"b":50.52538601346569,"d":50.78657485494268},"fa":{"b":17.58736379609377,"d":18.27400930390627}},"zoom":10,"center":{"jb":50.65616198748283,"kb":17.93068655000002}}', 
+    'map' => '{"bounds":{"ea":{"b":-70.36767265248001,"d":85.11399811539391},"ia":{"b":-180,"d":180}},"zoom":1,"center":{"lb":37.18446862061821,"mb":19.53367485000003}}', 
     'mail' => 'example@example.com'
 );
 
