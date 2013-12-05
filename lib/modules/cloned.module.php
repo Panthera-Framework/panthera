30b87500b2bf96f4a36f9b2dcf151941a6b6d481
Mateusz Warzyñski <lxnmen@gmail.com>
2013-12-05 21:22:21 +0100

    Cloned uses our httplib module now

--- lib/modules/cloned.module.php
+++ lib/modules/cloned.module.php
@@ -13,8 +13,8 @@ if (!defined('IN_PANTHERA'))
   
 global $panthera;
 require_once PANTHERA_DIR. '/share/phpQuery.php';
-require_once PANTHERA_DIR. '/share/httpful/src/Httpful/Bootstrap.php';
 $panthera -> importModule('simpleimage');
+$panthera -> importModule('httplib');
 
 /**
   * Mass content ripping module class
@@ -350,10 +350,11 @@ class cloned_images extends cloned_plugin
             /* OPTIONS WITH REQUIRED DOWNLOAD */
             if ($requiresDownload == True)
             {
-                \Httpful\Bootstrap::init();
+                $httplib = new httplib;
                 
                 try {
-                    $response = \Httpful\Request::get($src)/*->addOnCurlOption(CURLOPT_MUTE, True)*/->timeout(5)->sendIt();
+                    $httplib->timeout = 8;
+                    $response = $httplib->request($src);
                     $image = new SimpleImage();
                     $image -> loadFromString($response->body);
 
