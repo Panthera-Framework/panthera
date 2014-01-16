8cd864bf844160f022b9f114ef34041ed92b367f
webnull <webnull.www@gmail.com>
2014-01-16 16:29:58 +0100

    Added err404.url configuration variable, and some small fixes to custom pages action controller

--- lib/pages/custom.php
+++ lib/pages/custom.php
@@ -9,6 +9,11 @@
 
 $panthera -> importModule('custompages');
 $mode = 'fallback';
+$cpage = null;
+
+// to be set manually
+if (!isset($templateFile))
+    $templateFile = 'custom.tpl';
 
 if (isset($_GET['forceNativeLanguage']))
     $mode = 'forceNative';
@@ -16,22 +21,23 @@ if (isset($_GET['forceNativeLanguage']))
 if (isset($_GET['url_id']))
 {
     $cpage = customPage::getBy('url_id', $_GET['url_id'], '', $mode);
+    
 } elseif(isset($_GET['id'])) {
     $cpage = new customPage('id', $_GET['id']);
+    
 } elseif(isset($_GET['unique'])) {
     $cpage = customPage::getBy('unique', $_GET['unique'], '', $mode);
 }
 
-if ($cpage == null)
-    pa_redirect('?404');
-    
-if (!$cpage->exists())
-    pa_redirect('?404');
-
+if (!$cpage or !$cpage->exists())
+{
+    pa_redirect($panthera -> config -> getKey('err404.url', '?404', 'string', 'errors'));
+}
+   
 $tags = unserialize($cpage->meta_tags);
 $panthera -> template -> putKeywords($tags);
 $panthera -> template -> setTitle($cpage->title);
 $panthera -> template -> push('custom_name', $cpage->title);
 $panthera -> template -> push('custom_content', $cpage->html);
-$panthera -> template -> display('custom.tpl');
+$panthera -> template -> display($templateFile);
 pa_exit();
