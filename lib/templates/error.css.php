0ce953e683f6fd71c508a2c9c3faf33721924878
webnull <webnull.www@gmail.com>
2014-01-17 19:09:14 +0100

    Changed error pages to "django style"

--- lib/templates/error.css.php
+++ lib/templates/error.css.php
@@ -5,83 +5,28 @@ if (!defined('IN_PANTHERA'))
 
 ?>
 
-<style>
-            body {
-                background: -webkit-gradient(linear, left top, left bottom, from(rgb(0, 0, 0)), to(rgb(111, 111, 111))); /* Chromium/Chrome/Safari - all webkit based browsers */
-                background: -moz-linear-gradient(top,  rgb(0, 0, 0),  rgb(111, 111, 111)); /* Firefox - all gecko based browsers. */
-                filter: progid:DXImageTransform.Microsoft.gradient(startColorstr=rgb(0, 0, 0), endColorstr=rgb(111, 111, 111)); /* Internet Explorer */
-                background-image: -o-linear-gradient(rgb(0, 0, 0),rgb(111, 111, 111)); /* Opera */
-                background-repeat:repeat-x, repeat-y;
-
-                /*height: 1024px;*/
-            }
-
-            .info {
-                color: #00529B;
-                background: #BDE5F8;
-            }
-
-            .warning, .error, .info, .success {
-                border: 1px solid;
-                margin: 15px 0px;
-                padding:15px 20px 15px 55px;
-                width: 500px;	
-                font: bold 12px verdana;
-                -moz-box-shadow: 0 0 5px #888;
-                -webkit-box-shadow: 0 0 5px#888;
-                box-shadow: 0 0 5px #888;
-                text-shadow: 2px 2px 2px #ccc;
-                -webkit-border-radius: 15px;
-                -moz-border-radius: 15px;
-                border-radius: 15px;
-                width: 92%;
-            }
-
-            .warning {
-                color: #9F6000;
-                background: #FEEFB3;
-            }
-
-            .error {
-                color: rgb(27, 22, 2);
-                background: #FFBABA;
-            }
-
-            .success {
-                color: #4F8A10;
-                background: #DFF2BF;
-            }
-
-            .msg {
-                margin: 100px;
-            }
-
-            .content {
-                background: rgb(255, 236, 236);
-                margin: 0px auto;
-                width: 960px;
-                -moz-border-radius: 10px;
-                -webkit-border-radius: 10px;
-                border-radius: 10px;
-                -moz-box-shadow: 0px 0px 10px #422A20;
-                -webkit-box-shadow: 0px 0px 10px #422A20;
-                box-shadow: 0px 0px 10px #422A20;
-                padding: 30px 40px;
-                padding-top: 5px;
-                color: black;
-            }
-
-            .err_header {
-                margin-bottom: 5px;
-                color: black;
-                font-size: 35px;
-            }
-
-            .class_name {
-                color: rgb(58, 1, 1);
-            }
-
-            .func_name {
-                color: rgb(32, 32, 209)
-            }
-        </style>
+            <style>
+            html * { padding:0; margin:0; }
+            body * { padding:10px 20px; }
+            body * * { padding:0; }
+            body { font:small sans-serif; background:#eee; }
+            body>div { border-bottom:1px solid #ddd; }
+            h1 { font-weight:normal; margin-bottom:.4em; }
+            h1 span { font-size:60%; color:#666; font-weight:normal; }
+            table { border:none; border-collapse: collapse; width:100%; }
+            td, th { vertical-align:top; padding:2px 3px; }
+            th { width:12em; text-align:right; color:#666; padding-right:.5em; }
+            #info { background:#f6f6f6; }
+            #info ol { margin: 0.5em 4em; }
+            #info ol li { font-family: monospace; }
+            #summary { background: #ffffcc; padding-top: 25px; padding-left: 50px; padding-bottom: 25px; }
+            #summary h1 { font-size: 24px; font-weight: 700;}
+            #summaryDetails { list-style-type: none; padding-left: 25px; font-size: 16px; }
+            
+            .inner { padding-left: 25px; padding-bottom: 0px; }
+            
+            #explanation { background:#eee; border-bottom: 0px none; }
+            div { padding-left: 50px; padding-bottom: 25px; }
+            .lighter { background: #f6f6f6; }
+            .footer { padding-bottom: 100px; }
+            </style>
