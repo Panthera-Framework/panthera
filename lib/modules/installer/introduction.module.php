14c6e351f6f4b6010ad1f140796ffa4640fd98a8
Damian Kęska <webnull.www@gmail.com>
2014-03-25 23:00:00 +0100

    Introduction step for installer

---
+++ lib/modules/installer/introduction.module.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Introduction step
+ * 
+ * @package Panthera\installer
+ * @author Damian Kęska
+ * @license GNU Lesser General Public License 3, see license.txt
+ */
+
+ /**
+ * Introduction step
+ * 
+ * @package Panthera\installer
+ * @author Damian Kęska
+ */
+ 
+class introductionInstallerControllerSystem extends installerController
+{
+    // licence.required - use $_POST['licence_agree'] field with value "1" in HTML form
+    protected $config = array(
+        'licence.required' => array(
+            'value' => FALSE, 'type' => 'bool', 'description' => array('Requires user to accept licence or just mark checkbox', 'installer'),
+        ),
+    );
+    
+    /**
+     * Main function to display everything
+     * 
+     * @author Damian Keska
+     * @return null
+     */
+    
+    public function display()
+    {
+        if (!$this->config['licence.required']))
+            $this -> installer -> enableNextStep();
+        else {
+            if (isset($_POST['licence_agree']))
+            {
+                if (intval($_POST['licence_agree']) === 1)
+                {
+                    $this -> installer -> enableNextStep();
+                    $this -> installer -> goToNextStep();
+                }
+            }   
+        }
+        
+        //$this -> installer -> setButton('back', False);
+        $this -> installer -> template = 'index';
+    }
+}
