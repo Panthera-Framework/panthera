8522b24aec7a62d7d398bccc5e1d3b2a2212eabd
Mateusz Warzyñski <lxnmen@gmail.com>
2014-04-03 02:25:08 +0200

    Rewrited to object system - frontcontrollers ajaxpage

---
+++ lib/ajaxpages/admin/frontcontrollers.Controller.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+  * Show list of ajax pages
+  *
+  * @package Panthera\core\ajaxpages
+  * @author Damian Kęska
+  * @author Mateusz Warzyński
+  * @license GNU Affero General Public License 3, see license.txt
+  */
+
+
+/**
+  * Ajaxpages list pageController
+  *
+  * @package Panthera\core\ajaxpages
+  * @author Damian Kęska
+  * @author Mateusz Warzyński
+  */
+  
+class frontcontrollersAjaxControllerCore extends pageController
+{
+    protected $uiTitlebar = array(
+        'Index of front controllers', 'ajaxpages'
+    );
+    
+    protected $permissions = 'can_see_front_controllers';
+    
+    
+    public function display()
+    {
+        // scan both lib and content
+        $files = scandir(SITE_DIR);
+        
+        $controllers = array();
+        
+        foreach ($files as $file)
+        {
+            $pathinfo = pathinfo($file);
+        
+            if (strtolower($pathinfo['extension']) != 'php')
+                continue;
+        
+            if (!is_file($file))
+                continue;
+        
+            $name = basename($file);
+            $linked = False;
+            
+            if (is_link($file))
+            {
+                if (stripos(readlink($file), '/frontpages') !== False)
+                    $linked = True;
+            }
+            
+        
+            $controllers[] = array(
+                'name' => $name,
+                'linked' => $linked,
+                'modtime' => date($this -> panthera -> dateFormat, filemtime($file))
+            );
+        }
+        
+        $controllers = $this->panthera->get_filters('pa.frontcontrollers.list', $controllers);
+        $this -> panthera -> template -> push('list', $controllers);
+        
+        return $this -> panthera -> template -> compile('frontcontrollers.tpl');
+    }
+
+}
