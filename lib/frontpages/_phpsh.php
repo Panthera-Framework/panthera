928356183a34a660934bcbcde145bb2271a93be4
webnull <webnull.www@gmail.com>
2014-04-05 16:21:19 +0200

    Improved phpsh support (objective controller, hashtag, welcome text)

--- lib/frontpages/_phpsh.php
+++ lib/frontpages/_phpsh.php
@@ -1,26 +1,101 @@
+#!/usr/bin/env phpsh
 <?php
 /**
-  * Front controller for CLI app - phpsh
-  *
-  * @package Panthera\core
-  * @author Damian Kęska
-  * @see http://www.phpsh.org/
-  * @license GNU Affero General Public License 3, see license.txt
-  */
+ * Front controller for CLI app - phpsh
+ *
+ * @package Panthera\core\frontcontrollers
+ * @author Damian Kęska
+ * @see http://www.phpsh.org/
+ * @license GNU Affero General Public License 3, see license.txt
+ */
   
 # include app config and libs
 require_once 'content/app.php';
+include getContentDir('pageController.class.php');
 
 if (PANTHERA_MODE == 'cgi')
-    die('This script is not avaliable in CGI mode.');
+    die('This controller is not available in CGI mode.');
 
-error_reporting(E_ALL);
+/**
+ * Front controller for CLI app - phpsh
+ *
+ * @package Panthera\core\frontcontrollers
+ * @author Damian Kęska
+ * @see http://www.phpsh.org/
+ * @license GNU Affero General Public License 3, see license.txt
+ */
 
-// print logging output directly to console
-$panthera -> logging -> printOutput = True;
+class _phpshControllerSystem extends pageController
+{
+    /**
+     * Set error reporting and printing to console
+     * 
+     * @return null
+     */
+    
+    protected function setReporting()
+    {
+        error_reporting(E_ALL);
 
-// print generated output before setting printOutput to True
-print($panthera->logging->getOutput());
+        // print logging output directly to console
+        $this -> panthera -> logging -> printOutput = True;
+    }
+    
+    /**
+     * Prints welcome text
+     * 
+     * @return null
+     */
+    
+    protected function printWelcomeText()
+    {
+        $out = "\n* Panthera Framework components:\n";
+        $out .= "=> \$panthera -> user\n";
+        $out .= "=> \$panthera -> session\n";
+        $out .= "=> \$panthera -> config\n";
+        $out .= "=> \$panthera -> db\n";
+        $out .= "=> \$panthera -> logging\n";
+        $out .= "=> \$panthera -> locale\n";
+        $out .= "=> \$panthera -> template\n";
+        $out .= "=> \$panthera -> outputControl\n";
+        $out .= "=> \$panthera -> routing\n";
+        $out .= "=> \$panthera -> types\n";
+        
+        $out .= "\n\n* Useful functions:\n=> object_info(), r_dump(), object_dump(), var_dump(), print_r(), getContentDir(), pantheraUrl(), libtemplate::webrootMerge()";
+        
+        // make some space before prompt
+        $out .= "\n\n";
+        
+        $this -> panthera -> logging -> output($out, 'phpsh');
+    }
+    
+    /**
+     * Default action
+     * 
+     * @return null
+     */
+    
+    public function display()
+    {
+        $this -> setReporting();
+        
+        // print generated output before setting printOutput to True
+        print($this -> panthera->logging->getOutput());
+        
+        if (is_file('content/phpsh.rc.php'))
+        {
+            $this -> panthera -> logging -> output('Including content/phpsh.rc.php', 'phpsh');
+            include 'content/phpsh.rc.php';
+        }
+        
+        $this -> printWelcomeText();
+    }
+}
 
-if (is_file('content/phpsh.rc.php'))
-   include 'content/phpsh.rc.php';
+if (strpos(__FILE__, PANTHERA_DIR) !== FALSE)
+{
+    $object = new _phpshControllerSystem();
+    $object -> display();
+}
+
+$panthera = pantheraCore::getInstance();
