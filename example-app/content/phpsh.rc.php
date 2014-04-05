cf9750a47c40159ed631cd4990ffc819d15eddf8
webnull <webnull.www@gmail.com>
2014-04-05 16:21:31 +0200

    Added example phpsh.rc.php

---
+++ example-app/content/phpsh.rc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * RC file for _phpsh front controller - use this file to add custom startup PHP code
+ *
+ * @package Panthera\core\frontcontrollers
+ * @author Damian Kęska
+ * @see http://www.phpsh.org/
+ * @license GNU Affero General Public License 3, see license.txt
+ */
+ 
+// do something on _phpsh.php startup
