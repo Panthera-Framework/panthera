61ebc1e3590e080d604f2e1209ef2df659254603
webnull <webnull.www@gmail.com>
2014-04-14 15:33:30 +0200

    Added pageController -> getFeature() function that extends plugins support. It automaticaly calls a function in forked class and all hooked functions connected via Panthera hooking.

--- lib/pageController.class.php
+++ lib/pageController.class.php
@@ -409,4 +409,29 @@ abstract class pageController extends pantheraClass {
         if ($name)
             return new $name;
     }
+    
+    /**
+     * Execute hooks and defined functions with name $featureName
+     * Example: $featureName = 'custompages.add' will execute $this->custompages_addFeature($args, $additionalInfo) and $this->panthera->get_filters($featureName, $args, $additionalInfo)
+     * 
+     * @param string $featureName Hook and function name
+     * @param mixed $args Args to pass to function and/or hook
+     * @param mixed $additionalInfo Additional informations
+     * @param bool $fixOnFail Don't loose arguments data if any hook will fail (return false or null)
+     * @return $args Mixed arguments
+     */
+    
+    public function getFeature($featureName, $args='', $additionalInfo=null, $fixOnFail=True)
+    {
+        $f = preg_replace('/[^\da-zA-Z0-9]/i', '_', $featureName). 'Feature';
+        
+        if (method_exists($this, $f))
+        {
+            $args = $this->$f($args, $additionalInfo);
+        }
+        
+        $args = $this -> panthera -> get_filters($featureName, $args, $fixOnFail, $additionalInfo);
+        
+        return $args;
+    }
 }
