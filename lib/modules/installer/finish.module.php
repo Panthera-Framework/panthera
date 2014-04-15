cd5a31451cd2269d05f3bfb81d58fd7726ac3d60
webnull <webnull.www@gmail.com>
2014-04-16 01:45:46 +0200

    Moved routing data from finish step to new routing step

--- lib/modules/installer/finish.module.php
+++ lib/modules/installer/finish.module.php
@@ -15,16 +15,6 @@ if (!defined('PANTHERA_INSTALLER'))
 global $panthera;
 global $installer;
 
-// add routes to modules provided with Panthera
-$panthera -> routing -> map('GET|POST', 'contact', array('front' => 'index.php', 'GET' => array('display' => 'contact')), 'contact');
-$panthera -> routing -> map('GET|POST', 'pu[n:forceNative]?/[*:url_id].html', array('front' => 'index.php', 'GET' => array('display' => 'custom')), 'custom_url_id');
-$panthera -> routing -> map('GET|POST', 'pi[n:forceNative]?/[i:id].html', array('front' => 'index.php', 'GET' => array('display' => 'custom')), 'custom_id');
-$panthera -> routing -> map('GET|POST', 'pq[n:forceNative]?/,[i:unique].html', array('front' => 'index.php', 'GET' => array('display' => 'custom')), 'custom_unique');
-$panthera -> routing -> map('GET|POST', 'facebook/#[*:back]', array('front' => 'index.php', 'GET' => array('display' => 'facebook.connect')), 'facebook_connect');
-$panthera -> routing -> map('GET|POST', 'register', array('front' => 'index.php', 'GET' => array('display' => 'register')), 'register');
-$panthera -> routing -> map('GET|POST', 'login', array('front' => 'pa-login.php'), 'login');
-$panthera -> routing -> map('GET|POST', 'index.[html|py|pyc|rb]', array('redirect' => '', 'code' => 301), 'index-html');
-
 
 if ($panthera -> config -> getKey('requires_instalation') == True)
 {
---
+++ lib/modules/installer/routing.module.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Routing configuration step
+ * 
+ * @package Panthera\installer
+ * @author Damian Kęska
+ * @license GNU Lesser General Public License 3, see license.txt
+ */
+
+ /**
+ * Routing configuration step
+ * 
+ * @package Panthera\installer
+ * @author Damian Kęska
+ */
+
+class routingInstallerControllerSystem extends installerController
+{
+    /**
+     * Main function to display everything
+     * 
+     * @feature installer.routing null Install additional routes
+     * 
+     * @author Damian Keska
+     * @return null
+     */
+    
+    public function display()
+    {
+        // add routes to modules provided with Panthera
+        $this -> panthera -> routing -> map('GET|POST', 'contact', array('front' => 'index.php', 'GET' => array('display' => 'contact')), 'contact');
+        $this -> panthera -> routing -> map('GET|POST', 'pu[n:forceNative]?/[*:url_id].html', array('front' => 'index.php', 'GET' => array('display' => 'custom')), 'custom_url_id');
+        $this -> panthera -> routing -> map('GET|POST', 'pi[n:forceNative]?/[i:id].html', array('front' => 'index.php', 'GET' => array('display' => 'custom')), 'custom_id');
+        $this -> panthera -> routing -> map('GET|POST', 'pq[n:forceNative]?/,[i:unique].html', array('front' => 'index.php', 'GET' => array('display' => 'custom')), 'custom_unique');
+        $this -> panthera -> routing -> map('GET|POST', 'facebook/#[*:back]', array('front' => 'index.php', 'GET' => array('display' => 'facebook.connect')), 'facebook_connect');
+        $this -> panthera -> routing -> map('GET|POST', 'register', array('front' => 'index.php', 'GET' => array('display' => 'register')), 'register');
+        $this -> panthera -> routing -> map('GET|POST', 'login', array('front' => 'pa-login.php'), 'login');
+        $this -> panthera -> routing -> map('GET|POST', 'index.[html|py|pyc|rb]', array('redirect' => '', 'code' => 301), 'index-html');
+        
+        $this -> getFeature('installer.routing');
+        
+        // generate table log
+        $routes = array();
+        
+        foreach ($this -> panthera -> routing -> getRoutes() as $route => $data)
+            $routes[] = array($route, $data[0], $data[1]);
+        
+        
+        $this -> installer -> enableNextStep();
+        $this -> panthera -> template -> push('spinnerStepMessage', localize('Installing default URL routes...', 'installer'));
+        $this -> panthera -> template -> push('spinnerStepTable', $routes);
+        $this -> installer -> template = 'spinnerStep';
+    }
+}
