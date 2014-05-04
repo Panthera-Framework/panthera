e4a086cb18fb2a78b40051283e099c36679a7c60
Mateusz Warzyñski <lxnmen@gmail.com>
2014-05-05 01:30:58 +0200

    Deleted cloned database template - it is moved to separate repository

--- lib/database/templates/cloned.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `{$db_prefix}cloned`;
-
-CREATE TABLE IF NOT EXISTS `{$db_prefix}cloned` (
-  `id` mediumint(10) NOT NULL AUTO_INCREMENT,
-  `hash` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`)
-  UNIQUE KEY `hash` (`hash`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
