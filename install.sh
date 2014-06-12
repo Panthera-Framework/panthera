0110f78a564deda9f3a8468338b98e53913a42ef
webnull <webnull.www@gmail.com>
2014-06-12 23:08:33 +0200

    Cleanup in 3rdparty libraries

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ mkdir lib/share
 
 echo "Moving FirePHP"
 mv vendor/firephp/firephp-core lib/share/firephp-core --force
+rm lib/share/firephp-core/examples -rf
+rm lib/share/firephp-core/tests -rf
+rm lib/share/firephp-core/workspace -rf
 
 echo "Moving phpQuery"
 mv vendor/electrolinux/phpquery/phpQuery/* lib/share/ --force
@@ -17,24 +20,36 @@ mv vendor/facebook/php-sdk lib/share/facebook-php-sdk --force
 
 echo "Moving cron-expression"
 mv vendor/mtdowling/cron-expression lib/share/cron-expression --force
+rm lib/share/cron-expression/tests -rf
+rm lib/share/facebook-php-sdk/tests -rf
 
 echo "Moving httpful"
 mv vendor/nategood/httpful lib/share/httpful --force
 
 echo "Moving phpmailer"
 mv vendor/phpmailer/phpmailer lib/share/phpmailer --force
+rm lib/share/phpmailer/examples -rf
+rm lib/share/phpmailer/docs -rf
+rm lib/share/phpmailer/test -rf
 
 echo "Moving RainTPLv3"
 mv vendor/rain/raintpl lib/share/raintpl3  --force
+rm lib/share/raintpl3/.git -rf
+rm lib/share/raintpl3/example*
+rm lib/share/raintpl3/templates -rf
 
 echo "Moving password_compat"
 mv vendor/ircmaxell/password-compat lib/share/password-compat --force
+rm lib/share/password-compat/.git -rf
 
 echo "Moving Mobile-Detect"
 mv vendor/mobiledetect/mobiledetectlib lib/share/mobiledetectlib --force
+rm lib/share/mobiledetectlib/tests -rf
 
 echo "Moving mpdf"
 mv vendor/mpdf/mpdf lib/share/mpdf --force
+rm -rf lib/share/mpdf/examples
+rm -rf lib/share/mpdf/.git
 
 echo `pwd`
 
