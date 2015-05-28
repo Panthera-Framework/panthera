25a18de8a749c5c3f0b97345703be974854a1123
Damian Kęska <webnull.www@gmail.com>
2015-05-28 21:00:30 +0200

    Fixes to custom_pages table structure for SQLite3

--- lib/database/templates/sqlite3/custom_pages.sql
+++ lib/database/templates/sqlite3/custom_pages.sql
@@ -1,22 +1,23 @@
 DROP TABLE IF EXISTS `{$db_prefix}custom_pages`;
 
 CREATE TABLE "{$db_prefix}custom_pages" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT ,
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
   "unique" VARCHAR NOT NULL,
   "url_id" VARCHAR NOT NULL,
   "title" VARCHAR NOT NULL,
-  "meta_tags" VARCHAR NOT NULL,
-  "html" text NOT NULL,
+  "meta_tags" VARCHAR NULL,
+  "html" text NULL,
   "author_name" VARCHAR NOT NULL,
   "author_id" INTEGER NOT NULL,
   "language" VARCHAR NOT NULL DEFAULT 'polski',
-  "created" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "mod_author_name" VARCHAR NOT NULL,
-  "mod_author_id" INTEGER NOT NULL,
-  "mod_time" timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  "admin_tpl" VARCHAR NOT NULL,
-  "description" VARCHAR NOT NULL,
-  "image" VARCHAR NOT NULL  
+  "created" timestamp DEFAULT CURRENT_TIMESTAMP,
+  "mod_author_name" VARCHAR NULL,
+  "mod_author_id" INTEGER NULL,
+  "mod_time" timestamp DEFAULT '0000-00-00 00:00:00',
+  "admin_tpl" VARCHAR NULL,
+  "description" VARCHAR NULL,
+  "image" VARCHAR NULL,
+  "public" INT DEFAULT '0'
 );
 
 CREATE INDEX "{$db_prefix}custom_pages_url_id" ON "{$db_prefix}custom_pages" ("url_id");
