b27f3a6a20cb5cd38beaeaa6763eafcac3163e52
Mateusz Warzyński <lxnmen@gmail.com>
2015-06-27 18:26:07 +0200

    Added documentation for logging, added clear method - logging.

--- lib/modules/logging.class.php
+++ lib/modules/logging.class.php
@@ -6,25 +6,69 @@ namespace Panthera;
  * its used to debug whole application based on Panthera Framework
  *
  * @Package Panthera
- * @author Damian Kęska
+ * @author Damian Kęska <damian@pantheraframework.org>
+ * @author Mateusz Warzyński <lxnmen@gmail.com>
  */
 class logging extends baseClass
 {
-    public $buffer = array();
+    /**
+     * Store all messages that has been printed
+     *
+     * @var array $messages
+     */
+    public $messages = array();
+
+    /**
+     * Message format
+     *
+     * @var mixed|string $format
+     */
     public $format = '';
+
+    /**
+     * Date format used in message
+     *
+     * @var mixed|string $dateFormat
+     */
     public $dateFormat = '';
 
+    /**
+     * Turn on/off logging
+     *
+     * @var bool $enabled
+     */
+    public $enabled = false;
+
+    /**
+     * Init logging function
+     *
+     * @author Mateusz Warzyński <lxnmen@gmail.com>
+     */
     public function __construct()
     {
         parent::__construct();
         $this->format = $this->app->config->get('logging.format', '[%date][%path:%line] %message');
         $this->dateFormat = $this->app->config->get('logging.format.date', 'Y-m-d H:i');
+        $this->enabled = $this->app->config->get('logging.enabled', false);
     }
 
+    /**
+     *  Function that prints message
+     *
+     * @param string $message to print
+     *
+     * @author Mateusz Warzyński <lxnmen@gmail.com>
+     * @return bool|string
+     */
     public function output($message)
     {
+        if (!$this->enabled)
+        {
+            return false;
+        }
+
         $backtrace = debug_backtrace();
-        $backtrace = end($backtrace); // wtf
+        $backtrace = end($backtrace);
 
         $formatting = array(
             '%date'     => date($this->dateFormat),
@@ -42,8 +86,18 @@ class logging extends baseClass
             $this->format = str_replace($key, $value, $this->format);
         }
 
-        $this->buffer[] = $this->format;
+        $this->messages[] = $this->format;
 
         return $this->format;
+    }
+
+    /**
+     * Clear messages buffer
+     *
+     * @author Mateusz Warzyński <lxnmen@gmail.com>
+     */
+    public function clear()
+    {
+        $this->messages = array();
     }
 }
