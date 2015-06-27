33b9462da3eab6c7c8a560fdfa366d5a21da3a95
Mateusz Warzyński <lxnmen@gmail.com>
2015-06-27 16:16:32 +0200

    Added basic functionality to logging.

--- application/index.php
+++ application/index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
+
 require __DIR__. '/.content/app.php';
 
 print("Hello world");
@@ -13,3 +16,15 @@ var_dump($app->database->select('dbName', array('userName'), array(
 	'userName ASC',
 	'userID DESC',
 )));
+
+class Test
+{
+    public function __construct()
+    {
+        global $app;
+
+        $app->logging->output('test');
+    }
+}
+
+$t = new Test();
--- lib/modules/logging.class.php
+++ lib/modules/logging.class.php
@@ -10,5 +10,40 @@ namespace Panthera;
  */
 class logging extends baseClass
 {
+    public $buffer = array();
+    public $format = '';
+    public $dateFormat = '';
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->format = $this->app->config->get('logging.format', '[%date][%path:%line] %message');
+        $this->dateFormat = $this->app->config->get('logging.format.date', 'Y-m-d H:i');
+    }
+
+    public function output($message)
+    {
+        $backtrace = debug_backtrace();
+        $backtrace = end($backtrace); // wtf
+
+        $formatting = array(
+            '%date'     => date($this->dateFormat),
+            '%fullPath' => $backtrace['file'],
+            '%path'     => str_replace($this->app->appPath, '', $backtrace['file']),
+            '%line'     => $backtrace['line'],
+            '%basename' => basename($backtrace['file']),
+            '%function' => $backtrace['function'],
+            '%class'    => isset($backtrace['class']) ? $backtrace['class'] : '',
+            '%message'  => $message
+        );
+
+        foreach ($formatting as $key => $value)
+        {
+            $this->format = str_replace($key, $value, $this->format);
+        }
+
+        $this->buffer[] = $this->format;
+
+        return $this->format;
+    }
 }
