79f60b5b959bdf5b8384b8a15f547b6b0ed86123
Mateusz Warzyński <lxnmen@gmail.com>
2015-07-07 00:36:50 +0200

    Implemented indexing classes in files - indexService module.

--- lib/modules/indexService.class.php
+++ lib/modules/indexService.class.php
@@ -11,6 +11,11 @@ namespace Panthera;
  */
 class indexService extends baseClass
 {
+    public $libIndex = array();
+    public $appIndex = array();
+
+    public $parser = null;
+
     /**
      * Main function indexing files from lib and application directory.
      *
@@ -19,9 +24,8 @@ class indexService extends baseClass
      */
     public function indexDirectoriesAndFiles()
     {
-        $libIndex = $this->listIn($this->app->libPath, '', $this->app->libPath);
-        $appIndex = $this->listIn($this->app->appPath, '', $this->app->appPath);
-        return array('lib' => $libIndex, 'app' => $appIndex);
+        $this->libIndex = $this->listIn($this->app->libPath, '', $this->app->libPath);
+        $this->appIndex = $this->listIn($this->app->appPath, '', $this->app->appPath);
     }
 
     /**
@@ -57,5 +61,75 @@ class indexService extends baseClass
         }
 
         return $result;
+    }
+
+    /**
+     * Get classes from indexed files in lib root directory, needed in autoloader
+     *
+     * @return array
+     */
+    public function indexClasses()
+    {
+        $result = array();
+
+        if (empty($this->libIndex))
+        {
+            $this->indexDirectoriesAndFiles();
+        }
+
+        foreach ($this->libIndex as $filePath => $none)
+        {
+            if (strpos(basename($filePath), '.class.php') !== false)
+            {
+                $classContent = file_get_contents($filePath);
+
+                if ($classContent !== false)
+                {
+                    $result[$filePath] = $this->fileGetClasses($classContent);
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get list of declared class in PHP file (without including it)
+     *
+     * @param string $php_code
+     * @author Damian Kęska
+     * @author Mateusz Warzyński
+     * @author AbiusX <http://stackoverflow.com/questions/7153000/get-class-name-from-file>
+     * @return array
+     */
+    public static function fileGetClasses($php_code)
+    {
+        $classes = array();
+        $namespace = '';
+        $tokens = token_get_all($php_code);
+        $count = count($tokens);
+        for ($i=0; $i < $count; $i++)
+        {
+            if ($tokens[$i][0] === T_NAMESPACE)
+            {
+                for ($j=$i+1; $j<$count; ++$j)
+                {
+                    if ($tokens[$j][0]===T_STRING)
+                        $namespace .= "\\".$tokens[$j][1];
+                    elseif ($tokens[$j] === '{' || $tokens[$j] === ';')
+                        break;
+                }
+            }
+            if ($tokens[$i][0] === T_CLASS || $tokens[$i][0] === T_TRAIT)
+            {
+                for ($j=$i+1; $j<$count; ++$j)
+                {
+                    if ($tokens[$j] === '{')
+                        $classes[] = $namespace."\\".$tokens[$i+2][1];
+                }
+            }
+        }
+
+        return $classes;
     }
 }
