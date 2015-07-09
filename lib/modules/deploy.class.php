5fa729a48b2eae808140573c42d440373f5b811b
Mateusz Warzyński <lxnmen@gmail.com>
2015-07-10 01:10:49 +0200

    Initially added deployment module.

---
+++ lib/modules/deploy.class.php
@@ -0,0 +1,23 @@
+<?php
+namespace Panthera;
+
+/**
+ * All deployment functionality, strongly connected to cli as to provide comfortable
+ *      working Panthera Framework 2 shell.
+ *
+ * @package Panthera
+ * @author Mateusz Warzyński <lxnmen@gmail.com>
+ * @author Damian Kęska <damian@pantheraframework.org>
+ */
+class deploy extends baseClass
+{
+    /**
+     * Initialization of deployment module
+     *
+     * @author Mateusz Warzyński <lxnmen@gmail.com>
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
