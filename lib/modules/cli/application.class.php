85c111bc356ab14549ce8633f4a895c7ec1457e7
Damian Kęska <webnull.www@gmail.com>
2015-07-13 11:07:11 +0200

    CLI code improvements

--- lib/modules/cli/application.class.php
+++ lib/modules/cli/application.class.php
@@ -45,15 +45,56 @@ class application extends Panthera\baseClass
     }
 
     /**
-     * Standard help function
+     * Display help
      *
-     * @cli --help, -h
+     * @todo Optional and non-optional arguments support
+     * @cli optional
+     *
      * @param string $value Optional value
      * @author Damian Kęska <damian@pantheraframework.org>
      */
     public function help_cliArgument($value)
     {
+        $text = "Usage: " .basename($_SERVER['argv'][0]) . " [OPTIONS] [--ARGUMENTS]\n\n";
+        $reflection = new \ReflectionClass(get_called_class());
+        $authors = array();
 
+        foreach ($reflection->getMethods() as $method)
+        {
+            if (strpos($method->getName(), '_cliArgument') !== false)
+            {
+                $commandName = str_replace('_cliArgument', '', $method->getName());
+                $text .= "\t--" .$commandName;
+
+                foreach ($this->argumentsShort as $shortArg => $longArg)
+                {
+                    if ($longArg === $commandName)
+                    {
+                        $text .= ", -" .$shortArg;
+                        break;
+                    }
+                }
+
+                $comment = explode("\n", $method->getDocComment());
+                $text .= "\t" . ltrim($comment[1], ' * ');
+
+                foreach ($comment as $line)
+                {
+                    $line = ltrim($line, ' * ');
+
+                    if (strpos($line, '@author') === 0)
+                    {
+                        $authors[substr($line, 7)] = substr($line, 7);
+                    }
+                }
+            }
+        }
+
+        // list of application authors
+        $text .= "\n\nAuthors:\n" . implode("\n", array_keys($authors));
+
+        print($text. "\n");
+        exit;
     }
 
     /**
@@ -67,9 +108,17 @@ class application extends Panthera\baseClass
         exit;
     }
 
+    /**
+     * Call a method for shell argument
+     *
+     * @param string $function Function name eg. "help" or "list"
+     * @param int $i Index in $_SERVER['argv']
+     *
+     * @author Damian Kęska <damian@pantheraframework.org>
+     */
     public function cliArgumentsCallFunction($function, $i)
     {
-        $function = $function. '_cliArgument';
+        $function = str_replace('-', '_', $function). '_cliArgument';
         $value = '';
 
         if (method_exists($this, $function))
@@ -88,11 +137,40 @@ class application extends Panthera\baseClass
         }
     }
 
+    /**
+     * Case when no any arguments was specified to application eg. "$ deploy" (without arguments)
+     * Default show help command output
+     *
+     * @author Damian Kęska <damian@pantheraframework.org>
+     * @return null
+     */
+    public function cliArgumentsNoArgumentSpecified()
+    {
+        return $this->help_cliArgument(null);
+    }
+
+    /**
+     * Parse shell arguments and execute proper commands eg. --help will execute $this->help_cliArgument()
+     *
+     * @author Damian Kęska <damian@pantheraframework.org>
+     * @return null
+     */
     public function readArguments()
     {
+        if (count($_SERVER['argv']) === 1)
+        {
+            return $this->cliArgumentsNoArgumentSpecified();
+        }
+
         // go through all expressions in shell command eg. "deploy.php unit-tests --arg1 value" would be: ['deploy.php', 'unit-tests', '--arg1', 'value']
         foreach ($_SERVER['argv'] as $i => $arg)
         {
+            // don't parse our command path/basename
+            if ($i === 0)
+            {
+                continue;
+            }
+
             $value = '';
 
             /**
