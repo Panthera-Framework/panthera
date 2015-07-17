285db994cac395da32621f74621d211ad577f602
Damian Kęska <webnull.www@gmail.com>
2015-07-17 23:18:26 +0200

    Deployment - better dependencies recognition

--- lib/bin/deploy.php
+++ lib/bin/deploy.php
@@ -71,6 +71,66 @@ class deploymentApplication extends application
     }
 
     /**
+     * Verify task's dependencies recursively
+     *
+     * @param array $tasks List of tasks
+     * @param array $checked List of already checked tasks
+     * @param string $parentTask If current iterated task is a child, then there should be a parent
+     *
+     * @author Damian Kęska <damian@pantheraframework.org>
+     * @return null
+     */
+    public function verifyTasksDependencies($tasks, &$checked, $parentTask = '')
+    {
+        foreach ($tasks as $task)
+        {
+            if (in_array($task, $checked))
+            {
+                continue;
+            }
+
+            if (!isset($this->modules[$task]))
+            {
+                print("Error: Task \"" .$task. "\" does not exists");
+
+                if ($parentTask)
+                {
+                    print(", required by: \"" .$parentTask. "\"");
+                }
+
+                print("\n");
+                exit;
+            }
+
+            $object = $this->loadTaskModule($task);
+            $checked[] = $task;
+
+            if ($object->dependencies)
+            {
+                $this->verifyTasksDependencies($object->dependencies, $checked, $task);
+            }
+        }
+    }
+
+    /**
+     * Instantiate a task
+     *
+     * @param string $taskName
+     *
+     * @throws PantheraFrameworkException
+     * @throws \Panthera\FileNotFoundException
+     *
+     * @author Damian Kęska <damian@pantheraframework.org>
+     * @return \Panthera\deployment\task|object
+     */
+    protected function loadTaskModule($taskName)
+    {
+        require_once $this->app->getPath($this->modules[$taskName]);
+        $taskClass = "\\Panthera\\deployment\\" .basename($taskName). "Task";
+        return new $taskClass;
+    }
+
+    /**
      * Parse list of modules given from commandline and execute tasks
      *
      * @param \string[] $opts
@@ -83,6 +143,9 @@ class deploymentApplication extends application
      */
     public function parseOpts($opts)
     {
+        $checked = array();
+        $this->verifyTasksDependencies($opts, $checked);
+
         foreach ($opts as $moduleName)
         {
             // don't run same task again
@@ -91,31 +154,31 @@ class deploymentApplication extends application
                 continue;
             }
 
-            if (!isset($this->modules[$moduleName]))
-            {
-                print("Error: Task \"" .$moduleName. "\" does not exists\n");
-                exit;
-            }
-
             print("=======> Running task " .$moduleName. "\n");
 
-            require $this->app->getPath($this->modules[$moduleName]);
+            /**
+             * @var \Panthera\deployment\task $currentTask
+             */
+            $this->runnedTasks[$moduleName] = $currentTask = $this->loadTaskModule($moduleName);
 
-            $taskName = "\\Panthera\\deployment\\" .basename($moduleName). "Task";
-            $this->runnedTasks[$moduleName] = new $taskName;
-
-            if ($this->runnedTasks[$moduleName]->dependencies)
+            if ($currentTask->dependencies)
             {
-                $this->parseOpts($this->runnedTasks[$moduleName]->dependencies);
+                $this->parseOpts($currentTask->dependencies);
             }
 
-            if (!method_exists($this->runnedTasks[$moduleName], 'execute'))
+            if (!method_exists($currentTask, 'execute') && !$currentTask->dependencies)
             {
-                print("Error: Method execute does not exists in \"" .$moduleName. "\", cannot start task\n");
+                print("Error: Method execute does not exists in \"" .$moduleName. "\", cannot start task that even dont have any dependencies defined\n");
                 exit;
             }
 
-            $this->runnedTasks[$moduleName]->execute($this);
+            /**
+             * Execute a post-dependencies check/execution action
+             */
+            if (method_exists($currentTask, 'execute'))
+            {
+                $currentTask->execute($this);
+            }
         }
     }
 }
