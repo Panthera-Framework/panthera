476ffb65903b1053005e746d5fd0ce5f852dba04
Mateusz Warzyński <lxnmen@gmail.com>
2015-08-13 16:34:30 +0200

    Added cron testcase file.

---
+++ lib/tests/CronTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Panthera Framework 2 cron test cases
+ *
+ * @package Panthera\cron\tests
+ * @author Mateusz Warzyński <lxnmen@gmail.com>
+ */
+class CronTest extends PantheraFrameworkTestCase
+{
+
+}
