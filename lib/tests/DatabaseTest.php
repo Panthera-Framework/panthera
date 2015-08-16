9046493eaa58e083c7bff6a38c28041e1ea5dcd0
Mateusz Warzyński <lxnmen@gmail.com>
2015-08-17 01:33:45 +0200

    Added test cases (where, join) to database

--- lib/tests/DatabaseTest.php
+++ lib/tests/DatabaseTest.php
@@ -19,4 +19,27 @@ class DatabaseTest extends PantheraFrameworkTestCase
         $query = $this->app->database->select('dbName', array('userName'), array(), array('userName ASC', 'userID DESC'), array('userName', 'count(userId)'));
         $this->assertContains('SELECT s1.userName FROM `dbName` as s1  ORDER BY s1.userName ASC, s1.userID DESC GROUP BY s1.userName, count(s1.userId)', $query[0]);
     }
+
+    /**
+     * Test creating where condition query
+     *
+     * @author Mateusz Warzyński <lxnmen@gmail.com>
+     */
+    public function testWhereCondition()
+    {
+        $this->setup();
+        $whereCondition = $this->app->database->parseWhereConditionBlock(array('|=|test' => 'testValue'));
+        $this->assertEquals('( test = .testValue )', $whereCondition['sql']);
+    }
+
+    /**
+     * Test creating join condition query
+     *
+     * @author Mateusz Warzyński <lxnmen@gmail.com>
+     */
+    public function testJoinConditionBlock()
+    {
+        $this->setup();
+        $this->assertSame("JOIN group ON ( test = .testValue ) ", $this->app->database->parseJoinConditionBlock(array("JOIN|group" => array("|=|test" => 'testValue')), null));
+    }
 }
